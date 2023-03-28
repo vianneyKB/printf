609c5be0a3f5d5baf60dda640d0c8a6e80af4d20
vianneyKB <vianneykondo@yahoo.fr>
2023-03-28 15:19:46 -0700

    task 0 Write a function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-/**
-* _printf - Printf function
-* @format: format.
-* Return: Printed chars.
-*/
+void print_buffer(char buffer[], int *buff_ind);
 
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -50,6 +51,7 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
+
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
